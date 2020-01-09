26873043a53965eb799b73a82c01a4733adca9e0
Jure Babnik <jure.babnik@student.fmf.uni-lj.si>
2020-01-09 09:59:32 +0100

    Odstranil odvečne vrstice iz tabel

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -7,7 +7,7 @@ library(gsubfn)
 library(tidyr)
 library(shiny)
 library(xml2)
-library(proto)
+
 
 #sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
@@ -15,14 +15,21 @@ library(proto)
 player.data <- read.csv("nba_data\\player_data.csv")
 players <- read.csv("nba_data\\Players.csv", na="")
 seasons.stats <- read.csv("nba_data\\Seasons_Stats.csv")
-players$X = NULL
-seasons.stats$X = NULL
+players$X <- NULL
+seasons.stats$X <- NULL
 
 players.rodovniski <- na.omit(players, invert=FALSE)
 annual.totals <- seasons.stats %>% 
   select(Year, Player, Age, G, MP, FG, FGA, X3P, X3PA, X2P, 
-         X2PA, FT, FTA, ORB, DRB, TRB, AST, BLK, TOV, STL) %>%
+         X2PA, FT, FTA, ORB, DRB, TRB, AST, BLK, TOV, STL, FG., X3P., X2P., FT.) %>%
   na.omit()
+colnames(annual.totals) <- c("Year", "Player", "Age", "GP", "MP", "FG", "FGA", "3P", "3PA", "2P", "2PA", "FT",
+                             "FTA", "ORB", "DRB", "TRB", "AST", "BLK", "TOV", "STL", "FG%", "3P%", "2P%", "FT%")
+
+annual.totals <- annual.totals %>% group_by(Player, Year) %>% filter(row_number() == 1)
+
+
+
 
 #uvozimo iz spletne strani
 per.36.stats <- NULL
@@ -33,7 +40,16 @@ for (i in seq(1980, 2017, 1)) {
   tabelaena$Year <- i
   per.36.stats <- rbind(per.36.stats, tabelaena) 
 }
-  
+per.36.stats$GS <- NULL
+per.36.stats$Tm <- NULL
+na.omit(per.36.stats)
+per.36.stats <- per.36.stats %>% group_by(Player, Year) %>% filter(row_number() == 1)
+
+per.36.stats <- subset(per.36.stats, grepl('^\\d+$', per.36.stats$G))
+
+
+
+
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
 # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
