de4e8e8265198702e9780e129380768bc883d2d6
Babnik21 <jure.babnik@gmail.com>
2020-03-08 22:48:18 +0100

    Naredil shiny

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,23 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  
-  titlePanel("Slovenske občine"),
-  
-  tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
+  titlePanel("naslov"),
+  sidebarPanel(
+    selectInput(inputId  = "x_os", label = "Izberite kategorijo na x osi",
+                choices  = colnames(per.36.stats)[-c(1, 2, 31, 30)],
+                selected = NULL, multiple = FALSE, selectize = TRUE
+    ),
+    selectInput(inputId  = "y_os", label = "Izberite kategorijo na y osi",
+                choices  = colnames(per.36.stats)[-c(1, 2, 31, 30)],
+                selected = NULL, multiple = FALSE, selectize = TRUE
     )
+  ),
+  
+  mainPanel(
+    plotOutput("graf")
+  )
+  
+  
+  
+  
 ))
